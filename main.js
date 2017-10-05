7eb7c1cc1edfeb63a20d56c7d3909865bb3a92f8
Arthurlpgc <arthurlpgcosta@gmail.com>
2017-10-05 20:13:48 -0300

    chore(main,index): generate main

---
+++ main.js
@@ -0,0 +1,18 @@
+while(true){
+	graph = new Graph(3);
+	graph.addEdge(0, 1);
+	graph.addEdge(1, 2);
+	graph.addEdge(2, 3);
+	answer={}
+	opt=new Optimal(graph)
+	answer['none']=opt.solve()
+	answer['edges']=graph.n
+	answer['nodes']=graph.m
+	answer['2aprox']=opt.solve()
+	answer['greedy']=opt.solve()
+	answer['2greedy']=opt.solve()
+	var ajax = new XMLHttpRequest();
+	ajax.open("POST", "162.243.157.230:5000/graph", true);
+	ajax.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	ajax.send(answer)
+}
