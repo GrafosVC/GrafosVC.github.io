df2b0a804b5bcb724ea00700fe39a8fc80dfaf6c
Arthurlpgc <arthurlpgcosta@gmail.com>
2017-10-05 20:19:07 -0300

    fix(main,index): fix url

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ while(true){
 	answer['2greedy']=opt.solve()
 	answer['graph']=""
 	var ajax = new XMLHttpRequest();
-	ajax.open("POST", "162.243.157.230:5000/graph", true);
+	ajax.open("POST", "http://162.243.157.230:5000/graph", true);
 	ajax.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 	ajax.send(answer)
 }
