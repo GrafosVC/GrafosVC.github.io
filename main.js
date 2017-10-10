34f05aa28d2a752ab40e61ffa6ae4a6c021fd336
Bolado <vtcb@cin.ufpe.br>
2017-10-10 13:16:48 -0300

    Update client to handle batch requests

--- main.js
+++ main.js
@@ -1,9 +1,32 @@
-for (var n = 5, k = 1; ; n++, k *= 2) {
-	for (var i = 0; i < k; i++) {
-		const min_m = n - 1;
-		const max_m = (n * (n - 1)) / 2;
-		const m = min_m + Math.floor(Math.random() * (max_m - min_m + 1));
-		const graph = Generator.generate(n);
+const SERVER = 'http://162.243.157.230:5000/';
+
+const getBatch = () => {
+	const ajax = new XMLHttpRequest();
+	ajax.open('GET', SERVER + 'graphindex');
+	ajax.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+	ajax.send();
+	return JSON.parse(ajax.responseText);
+};
+
+const sendOne = answer => {
+	const ajax = new XMLHttpRequest();
+	ajax.open('POST', SERVER + 'graph', true);
+	ajax.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+	ajax.send(JSON.stringify(answer));
+};
+
+const sendBatch = answers => {
+	answers.forEach(answers, answer => {
+		sendOne(answer);
+	});
+};
+
+const runBatch = (N, M) => {
+	const GRAPH_QTY = 69;
+	const answers = [];
+
+	for (var i = GRAPH_QTY - 1; i >= 0; i--) {
+		const graph = Generator.generate(N, M);
 		if (graph == null) continue;
 
 		const answer = {
@@ -15,9 +38,14 @@ for (var n = 5, k = 1; ; n++, k *= 2) {
 			'2greedy': (new RandomGreedySolver(graph)).solve(),
 			'graph':   graph.getRepresentation()
 		};
-		const ajax = new XMLHttpRequest();
-		ajax.open("POST", "http://162.243.157.230:5000/graph", true);
-		ajax.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
-		ajax.send(JSON.stringify(answer));
+
+		answers.push(answer);
 	}
+
+	return answers;
+};
+
+while (true) {
+	const batch = getBatch();
+	sendBatch(runBatch(batch.nodes, batch.edges));
 }
