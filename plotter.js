31653fafb7da7568dcb427864a5eca5e4f72549d
root <vtcb@cin.ufpe.br>
2017-10-15 01:09:55 +0000

    Add surface plot

--- plotter.js
+++ plotter.js
@@ -9,17 +9,20 @@ class Plotter {
   }
 
   plot(graphs) {
+    this.plotSurface(graphs)
+  }
+
+  plotBar(graphs) {
     const data = new vis.DataSet();
 
     var id = 0;
     for (const N in graphs) {
       for (const M in graphs[N]) {
-        console.log(N + ", " + M + ", " + graphs[N][M]['mxm2aprox'])
         data.add({
           id: id++,
-          x:  N * 5,
+          x:  N * 1,
           y:  M * 1,
-          z:  graphs[N][M]['mxm2aprox'],
+          z:  graphs[N][M]['cntgreedy'] / graphs[N][M]['cntnone'],
         });
       }
     }
@@ -31,8 +34,66 @@ class Plotter {
         showPerspective: true,
         showGrid: true,
         showShadow: false,
-        keepAspectRatio: true,
-        verticalRatio: 1,
+        keepAspectRatio: false,
+        xLabel: 'Nodes',
+        yLabel: 'Edges',
+        zLabel: 'Approximation',
+    };
+
+    const container = document.getElementById('curve');
+    const graph3d = new vis.Graph3d(container, data, options);
+  }
+
+  plotSurface(graphs) {
+    const data = new vis.DataSet();
+
+    var MAX_MAX_M = 0;
+    for (const N in graphs) {
+      const MAX_M = (N * (N - 1)) / 2;
+      MAX_MAX_M = Math.max(MAX_MAX_M, MAX_M);
+    }
+
+    var id = 0;
+    for (const N in graphs) {
+      const MAX_M = (N * (N - 1)) / 2;
+      const M_list = []
+      M_list.push([0, 1]);
+      M_list.push([N - 1, 1]);
+      for (const M in graphs[N]) {
+        M_list.push([M * 1, graphs[N][M]['cntgreedy'] / graphs[N][M]['cntnone']]);
+      }
+      M_list.push([MAX_M, 1]);
+      M_list.push([MAX_MAX_M, 1]);
+
+      const getMiddle = (a, b, c, d) => {
+        return (a * d + b * c) / (c + d);
+      };
+
+      var i = 0;
+      for (var M = 0; M <= MAX_MAX_M; M++) {
+        while (M_list[i][0] < M) i++;
+        const value = M_list[i][0] == M ? M_list[i][1] : getMiddle(
+            M_list[i - 1][1],
+            M_list[i][1],
+            M - M_list[i - 1][0],
+            M_list[i][0] - M);
+        data.add({
+          id: id++,
+          x:  N * 1,
+          y:  M * 1,
+          z:  value,
+        });
+      }
+    }
+
+    const options = {
+        width:  '600px',
+        height: '600px',
+        style: 'surface',
+        showPerspective: true,
+        showGrid: true,
+        showShadow: false,
+        keepAspectRatio: false,
         xLabel: 'Nodes',
         yLabel: 'Edges',
         zLabel: 'Approximation',
