ccedd48fb74ecd5de696848bcac0b0371dc98c39
root <vtcb@cin.ufpe.br>
2017-10-16 21:16:30 +0000

    Add graph control

--- plot.js
+++ plot.js
@@ -1,1 +1,7 @@
-window.onload = () => (new Plotter).run();
+const plotter = new Plotter();
+
+function plotGraph() {
+  plotter.runGraph();
+}
+
+window.onload = () => plotter.runAll();
--- plotter.js
+++ plotter.js
@@ -1,21 +1,43 @@
 class Plotter {
   constructor() {
     this.server = 'http://162.243.157.230:5000/';
+    this.graphData = this.getGraphData();
+    this.edgeFunction = [
+      N => N - 1,
+      N => 2 * N,
+      N => 3 * N,
+      N => Math.floor(N * N / 5),
+      N => Math.floor(N * N / 4)
+    ];
   }
 
-  run() {
-    const graphData = this.getGraphData();
-    this.plotCurve(graphData.graphs);
-    this.plotGraph(graphData.graphs['5']['10'].graph['greedy']);
+  runAll() {
+    this.runCurve();
+    this.runGraph();
+  }
+
+  runCurve() {
+    this.plotCurve(this.graphData.graphs);
+  }
+
+  runGraph() {
+    const nodes = document.getElementById('graph-nodes').value;
+    const edges_option = document.querySelector('input[name="graph-edges"]:checked').value;
+    const edges = this.edgeFunction[Number(edges_option)](nodes);
+    const data = this.graphData.graphs[nodes][edges];
+    if (!data) {
+      const container = document.getElementById('graph');
+      container.innerText = 'There is no data for these values.';
+    } else {
+      this.plotGraph(data.graph['greedy']);
+    }
   }
 
   plotGraph(graph) {
-    console.log(graph);
     const nodes = new vis.DataSet();
     const edges = new vis.DataSet();
 
     for (var i = Number(graph.n) - 1; i >= 0; i--) {
-      console.log("Node: " + i);
       nodes.add({
         id: i,
         label: String(i)
@@ -26,7 +48,10 @@ class Plotter {
       edges.add({from: graph.edges[i][0], to: graph.edges[i][1]});
     }
 
-    const options = {};
+    const options = {
+        width:  '600px',
+        height: '600px'
+    };
     const data = {
       nodes: nodes,
       edges: edges
